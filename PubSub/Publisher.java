83c33b8b3e505bc0fcf693d87df451eab02547ac
arush077 <arushshetty07@gmail.com>
2026-08-15 13:50:29 +0530

    updated code PubSub

--- PubSub/Publisher.java
+++ PubSub/Publisher.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+// Publisher class will have a sendMessageToServer(msg,topicName) : we need to send msg to a topicName so have both
+// so we are telling the server ki ye raha msg and ye raha the topic where u need to push the msg to.
 class Publisher{
     int id;
     Server server;
@@ -9,72 +11,109 @@ class Publisher{
         this.server = server;
     }
 
-    void sendMessageToServer(String msg){
-        server.recieveMessageFromPublisher(msg);
+    void sendMessageToServer(String msg,String topicName){
+        server.recieveMessageFromPublisher(msg,topicName);
     }
 }
 
+// Server will have a list of topics == so also keep a functionality to addTopic
+// While receiving from the Publisher recieveMessageFromPublisher(msg,topicName) then find the topic and add in its queue
 class Server{
-    Subscriber subscriber;    
+    List<Topic> topicList = new ArrayList<>();  
 
-    public Server(Subscriber subscriber) {
-        this.subscriber = subscriber;
-    }
-
-    void recieveMessageFromPublisher(String msg){
-        //Its receiving it from the Publisher, so add it in the queue
+    void recieveMessageFromPublisher(String msg,String topicName){
+        //Its receiving it from the Publisher, so add it in that topic ka queue
+        //Searching that topic in the topicList of the server
+        for(int i=0;i<topicList.size();i++){
+            if(topicList.get(i).topicName == topicName){
+                //found it 
+                Topic topic = topicList.get(i);
+                topic.addinQueue(msg);
+                break;
+            }
+        }
         System.out.println("Reached the server" + msg);
-        sendMessageToSubscriber(msg);
     }
 
-    //Lets now send this to the Subscriber
-    void sendMessageToSubscriber(String msg){
-        subscriber.recieveMessageFromServer(msg);
-    }  
+    // We need to add topics to the server
+    void addTopic(Topic newTopic){
+        topicList.add(newTopic);
+    }
+}
+
+// Topic will have a list of subscribers who can subscribe to it == so also keep a functionality to addSubscriber
+// addinQueue fn of the Topic acts as the reciever end for Server ka recieveMessageFromPublisher
+// sendMessageToAllSubscribers will send the queue elements to all the subscribers and pop it from queue
+class Topic{
+    int id;
+    String topicName;
+    public Topic(String topicName) {
+        this.topicName = topicName;
+    }
+
+   Queue<String> q = new ArrayDeque<>();
+   List<Subscriber> subscriberList = new ArrayList<>();
+
+
+   void addinQueue(String msg){
+        q.add(msg);
+   }
+
+    //Lets now send this to all the subscribed Subscriber
+    //No message parameter, kyuki queue ke peek me jobhi hoga wahi denge aur pop(remove) karenge na
+    void sendMessageToAllSubscribers(){
+        String msg = q.peek();
+        q.remove();
+
+        for(int i=0;i<subscriberList.size();i++){
+            Subscriber subscriber = subscriberList.get(i);
+            subscriber.recieveMessageFromTopic(msg);
+        } 
+    } 
+    
+    void addSubscriber(Subscriber newsubscriber){
+        subscriberList.add(newsubscriber);
+    }
 }
 
 class Subscriber{
     int id;
+    String name;
 
-    void recieveMessageFromServer(String msg){
+    public Subscriber(String name) {
+        this.name = name;
+    }
+
+    void recieveMessageFromTopic(String msg){
         System.out.println("Reached the Subscriber" + msg);
     }
 }
 
-
-
-class Topic{
-    int id;
-    String topicName;
-    List<MessageQueue> messageQueuesList = new ArrayList<>(); //each topic can have multiple queue
-}
-
-class MessageQueue{
-    int id;
-    Queue<Message> queue = new ArrayDeque<>();
-
-}
-
-class Message{
-    Publisher p;
-    Subscriber s;
-    String text;
-
-    Message(String input){
-        text = input;
-    }
-}
-
-
-
 class Main{
     public static void main(String[] args){
+        //Message banao
         String msg = "Hi, this is just a check";
 
-        Subscriber subscriber1 = new Subscriber();
-        Server server = new Server(subscriber1);
+        //Start writing from the end of the flow (ULTA)
+        // First make subsriber -->then make topic and add subscriber ---> then make server and add the topic ---> then make publisher
+       
+        // Making subscriber
+        Subscriber subscriber1 = new Subscriber("Arush");
+        
+        // Making topic and adding subscriber
+        Topic sportsTopic = new Topic("Sports");
+        sportsTopic.addSubscriber(subscriber1);
+
+         // Making topic and adding topics
+        Server server = new Server();
+        server.addTopic(sportsTopic);
+
+        // Making publisher
         Publisher publisher = new Publisher(10,server);
 
-        publisher.sendMessageToServer(msg);
+        //sendMessageToServer will send the msg to server which will push the msg in the queue of this "Sports" topic
+        //sendMessageToAllSubscribers will send all the queue elements from the topic to all the subscribers
+        publisher.sendMessageToServer(msg,"Sports");
+        sportsTopic.sendMessageToAllSubscribers();
     }
 }
---
+++ PubSub/readme.md
@@ -0,0 +1,29 @@
+```
+Publisher
+   │
+   │ has reference
+   ▼
+Server
+   │
+   │ has list of
+   ▼
+Topics
+   │
+   ├──────────────► Topic 1
+   │
+   ├──────────────► Topic 2
+   │
+   └──────────────► Topic 3
+                      │
+                      │ has list of
+                      ▼
+                  Subscribers
+                      │
+                      ├────────► Subscriber 1
+                      ├────────► Subscriber 2
+                      └────────► Subscriber 3
+
+
+```
+
+
